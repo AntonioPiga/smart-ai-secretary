1eab2ee5d819122fef10e56570467eacf7a04263
Antonio Piga <antonio.piga93@gmail.com>
2024-01-23 22:32:55 +0100

    feat: calendar events ai start

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -3,21 +3,48 @@
 	import { onMount } from 'svelte';
 	import { PUBLIC_URL_GET_CALENDAR_TOKEN } from '$env/static/public';
 	import { PUBLIC_URL_CALENDAR_ID } from '$env/static/public';
+	import { PUBLIC_URL_AI_DESCRIBE_EVENTS } from '$env/static/public';
 
 	var token = '';
+	var eventsDescription: object;
 
 	const authorizationUrl = generateAuthorizationUrl();
 	console.log('Authorization URL:', authorizationUrl);
+
 	async function callback() {
 		const queryParams = new URLSearchParams(window.location.search);
 		const code = queryParams.get('code');
 
-		if (code) {
-			token = getToken(code) as unknown as string;
-			console.log('token');
-			console.log(token);
+		if (code && token.length == 0) {
+			try {
+				token = await getToken(code);
+				console.log('token:', token);
+				var events = await getEvents(token);
+				var aiDescriptionEvents = await descriptionEvents(events);
+				eventsDescription = aiDescriptionEvents;
+			} catch (error) {
+				console.error('Error during token and events call:', error);
+			}
 		} else {
-			console.error('Error: code not exist');
+			console.error('Error: code does not exist');
+		}
+	}
+
+	async function descriptionEvents(events: any) {
+		const apiUrl = PUBLIC_URL_AI_DESCRIBE_EVENTS;
+
+		const params = {
+			input: events
+		};
+
+		try {
+			const response = await axios.post(apiUrl, params);
+			console.log('Response is:', response.data);
+			console.log('Response is:', response.data.token);
+			return response.data.output;
+		} catch (error) {
+			console.error('Error during token call:', error);
+			throw error;
 		}
 	}
 
@@ -41,32 +68,77 @@
 			code: code
 		};
 
-		axios
-			.post(apiUrl, params)
-			.then((response) => {
-				console.log('Response is:', response.data);
-				return response.data;
-			})
-			.catch((error) => {
-				console.error('Error during token call:', error);
-				throw error;
-			});
-
-		return 'error';
+		try {
+			const response = await axios.post(apiUrl, params);
+			console.log('Response is:', response.data);
+			console.log('Response is:', response.data.token);
+			return response.data.token;
+		} catch (error) {
+			console.error('Error during token call:', error);
+			throw error;
+		}
 	}
 
-	async function getEvents(token: string) {}
+	async function getEvents(token: string) {
+		const apiUrl = 'https://www.googleapis.com/calendar/v3/calendars/primary/events';
+
+		const headers = {
+			Authorization: 'Bearer ' + token
+		};
+
+		const currentDate = new Date();
+		currentDate.setHours(0, 0, 0);
+		const formattedStartDate = currentDate.toISOString().split('.')[0] + 'Z';
+
+		currentDate.setHours(23, 59, 59);
+		const formattedEndDate = currentDate.toISOString().split('.')[0] + 'Z';
+
+		const params = {
+			timeMin: formattedStartDate,
+			timeMax: formattedEndDate
+		};
+
+		try {
+			const response = await axios.get(apiUrl, { headers, params });
+
+			const items: Array<{
+				start: string;
+				end: string;
+				organizer: string;
+				summary: string;
+			}> = response.data.items.map((item: any) => {
+				return {
+					start: item.start.dateTime,
+					end: item.end.dateTime,
+					organizer: item.organizer.email,
+					summary: item.summary
+				};
+			});
+			return items;
+		} catch (error) {
+			console.error('Errore durante la chiamata agli eventi:', error);
+			throw error;
+		}
+	}
 
 	onMount(async () => {
 		const queryParams = new URLSearchParams(window.location.search);
 		const code = queryParams.get('code');
 
-		if (code) {
-			callback();
-		} else if (token.length > 0) {
-			getEvents(token);
+		if (code && token.length == 0) {
+			console.log('callback calling');
+			await callback();
 		}
 	});
 </script>
 
 <p>Ciao</p>
+<div class="h-50 border border-primary rounded bg-slate-200 shadow-md p-2">
+	{#if eventsDescription}
+		{eventsDescription}
+	{:else}
+		<div class="flex justify-center items-center h-30 p-10">
+			<div class="animate-spin rounded-full h-10 w-10 border-b-2 border-primary"></div>
+		</div>
+	{/if}
+</div>
